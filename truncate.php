fe96b4f978ba8ccf060432406dde50d6063a34fe
Rashid <alam.rashid039@gmail.com>
2016-12-04 15:21:10 +0000

    Tried removing unwanted files and log content

--- truncate.php
+++ truncate.php
@@ -1,21 +1,25 @@
 <?php
+require "vendor/autoload.php";
+error_reporting(E_ALL);
 define('the_add_log', TRUE);
-require_once("add_log.inc");
+//require_once("add_log.inc");
+
+$addlogobj =  new Flixmates\Config\ADDLOGS;
 $uniqid = uniqid();
 shell_exec("/var/www/html/flix_mates/truncate.sh");
-add_log("All data deleted ...", $uniqid);
+$addlogobj->add_log("All data deleted ...", $uniqid);
 echo "All data deleted ...<br/><br/>";
 
 array_map('unlink', glob("/var/www/html/flix_mates/images_upload/*"));
-add_log("All Image Files deleted from server.", $uniqid);
+$addlogobj->add_log("All Image Files deleted from server.", $uniqid);
 echo"All Image Files deleted ...<br/><br/>";
 
 array_map('unlink', glob("/var/www/html/flix_mates/videos_upload/*"));
-add_log("All Video Files deleted from server ...", $uniqid);
+$addlogobj->add_log("All Video Files deleted from server ...", $uniqid);
 echo"All Video Files deleted ...<br/><br/>";
 
 array_map('unlink', glob("/var/www/html/flix_mates/audios_upload/*"));
-add_log("All Audio Files deleted from server ...", $uniqid);
+$addlogobj->add_log("All Audio Files deleted from server ...", $uniqid);
 echo"All Audio Files deleted ...<br/><br/>";
 exit;
 ?>
